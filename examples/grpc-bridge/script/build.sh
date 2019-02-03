fce7740274993ebaa976f7085d0cf0ed9441016f
Yuto Suzuki <yuutoo.advance@gmail.com>
2019-02-03 09:59:57 +0900

    fix failed boot

--- examples/grpc-bridge/script/build.sh
+++ examples/grpc-bridge/script/build.sh
@@ -1,12 +1,15 @@
+#!/usr/bin/env bash
+
 cd $(dirname $0)/..
 
 opt=${1}
-if [ $opt == 'up' ]; then
+if [ "$opt" == 'up' ]; then
     docker-compose -p backend -f docker-compose.backend.yml up -d --build
     docker-compose -f docker-compose.front.yml up -d --build
-elif [ $opt == 'down' ]; then
+elif [ "$opt" == 'down' ]; then
     docker-compose -f docker-compose.front.yml down
     docker-compose -p backend -f docker-compose.backend.yml down
 else 
+    echo "need arg up or down"
     exit 1
 fi
